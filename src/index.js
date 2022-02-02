b2ae8c350ce95519d7a191579e927ebc5cf20b6d
Mauricio <shinji.256@gmail.com>
2022-02-01 18:40:12 -0600

    UI changes, New sections content, sidebar

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { MoralisProvider } from "react-moralis";
 import { MoralisDappProvider } from "./providers/MoralisDappProvider/MoralisDappProvider";
 import App from "./App";
 import "./index.css";
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-JMBP4KTF8F');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
 const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
